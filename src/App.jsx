7cc06981ffcd64d1d783871a95141dab950599dd
Marcelo Dantas <mahawprogramacao@gmail.com>
2024-03-03 12:27:39 -0300

    feat: Navbar component included

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <h2>MoviesLib</h2>
       <Outlet />
     </div>
   );
