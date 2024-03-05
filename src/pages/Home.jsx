3b0f6050386bca345d067ef84efadbaa0549aa95
Marcelo Dantas <mahawprogramacao@gmail.com>
2024-03-05 16:40:51 -0300

    feat: api requistion included

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,27 @@
+import { useState, useEffect } from "react";
+
+const moviesUrl = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
-    return <div>Home</div>
+    const [topMovies, setTopMovies] = useState([]);
+
+    const getTopRatedMovies = async (url) => {
+        const answer = await fetch(url);
+        const data = await answer.json();
+
+        setTopMovies(data.results);
+    }
+
+    useEffect(() => {
+        const topRatedUrl = `${moviesUrl}top_rated?${apiKey}`;
+
+        getTopRatedMovies(topRatedUrl);
+    }, [])
+
+    return (
+        <div>{topMovies && topMovies.map((movie) => <p key={movie.title}>{movie.title}</p>)}</div>
+    )
 }
 
 export default Home;
