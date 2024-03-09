159e60794255f0d0086f845694ad1c94a85a8d9a
Marcelo Dantas <mahawprogramacao@gmail.com>
2024-03-09 12:58:16 -0300

    feat: navbar styles included

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ import "./styles.css"
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Outlet />
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
