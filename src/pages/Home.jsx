25352698cd2b2e9127e2699e3833a76b247e4659
Marcelo Dantas <mahawprogramacao@gmail.com>
2024-03-10 13:09:54 -0300

    feat: movie card styles included

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import MovieCard from "../components/MovieCard";
+import "./styles.css"
 
 const moviesUrl = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
