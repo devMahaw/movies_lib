da5ae3d33f001fcb3a43b989ae38db580c4c3604
Marcelo Dantas <mahawprogramacao@gmail.com>
2024-03-11 17:36:45 -0300

    feat: search section included

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi"
+import { useState } from "react";
 import "./styles.css"
 
 const Navbar = () => {
+    const [search, setSearch] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!search) return;
+
+        navigate(`/search?q=${search}`);
+        setSearch("");
+    }
+
     return (
         <nav id="navbar">
             <h2>
@@ -10,8 +23,10 @@ const Navbar = () => {
                     <BiCameraMovie /> MoviesLib
                 </Link>
             </h2>
-            <form>
-                <input type="text" placeholder="Busque um filme" />
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Busque um filme" onChange={(e) =>
+                    setSearch(e.target.value)} value={search} />
+
                 <button type="submit">
                     <BiSearchAlt2 />
                 </button>
