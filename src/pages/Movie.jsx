ebcc9d9ff9a80377751665b34a4f08b1f8947246
Marcelo Dantas <mahawprogramacao@gmail.com>
2024-03-12 16:48:01 -0300

    feat: fetch movie

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,5 +1,36 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+//import { BsGraphUp, BsWallet2, BsHourglassSplit, BsFillFileEarmarkTextFill } from "react-icons/bs"
+import MovieCard from "../components/MovieCard";
+//import "styles.css"
+
+const moviesUrl = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Movie = () => {
-    return <div>Movie</div>
+    const { id } = useParams();
+    const [movie, setMovie] = useState(null);
+
+    const getMovie = async (url) => {
+        const answer = await fetch(url);
+        const data = await answer.json();
+
+        setMovie(data);
+    }
+
+    useEffect(() => {
+        const movieUrl = `${moviesUrl}${id}?${apiKey}`;
+        getMovie(movieUrl);
+    }, [])
+
+    return (
+        <div className="movie-page">
+            {movie && (<>
+                <MovieCard movie={movie} showLink={false} />
+                <p className="tag-line"></p>
+            </>)}
+        </div>
+    )
 }
 
 export default Movie;
